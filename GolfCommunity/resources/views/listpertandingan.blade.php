4465a1c4fef6ea3b6413f03409771f6e729787ad
rasyidrrd <avisheldoon@gmail.com>
2021-05-03 11:07:31 +0700

    daftar pertandingan belum fix

--- GolfCommunity/resources/views/listpertandingan.blade.php
+++ GolfCommunity/resources/views/listpertandingan.blade.php
@@ -2,25 +2,41 @@
 @section('judul_halaman', 'List Pertandingan')
 @section('konten')
 
-<div class="container">
+<div class="container py-5" style="min-height: 100vh">
     <h2 class="mt-4 text-center">List Pertandingan</h2>
-    <a href="{{ url('/pertandingan/buatp') }}" class="btn btn-success">Buat Pertandingan</a>
-    <div class="row row-cols-1 row-cols-md-2 g-4 my-4">
-      
-		@foreach ($pertandingans as $p)
-        
-        <div class="col">
-          <div class="card">
-            <img src="{{ URL::asset('images/upload/') }}/{{ $p->gambar }}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="{{ url('/pertandingan/buatp') }}" class="btn btn-success">Buat Pertandingan</a>
+
+            <div class="row mt-3">
+                @if (!$pertandingans->isEmpty())
+                    @foreach ($pertandingans as $key => $game)
+                    <div class="col-md-3">
+                            <div class="card" style="width: 100%;">
+                                <img src="{{ URL::asset('images/upload/') }}/{{ $game->gambar }}" class="card-img-top" alt="{{$game->nama}}"
+
+                                    style="object-fit: cover; height: 200px;">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $game->nama }}</h5>
+                                    <p class="card-text">
+                                        @if (strlen($game->deskripsi_singkat) > 100)
+                                            {{ substr($game->deskripsi_singkat, 0, 100) . '...' }}
+                                        @else
+                                            {{ $game->deskripsi_singkat }}
+                                        @endif
+                                    </p>
+                                    <p class ="card-text">Rp. {{ $game ->harga_tiket}} </p>
+                                    <div class="d-flex">
+                                        <a class="btn btn-warning" href='{{ url('/pertandingan/showpertandingan', $game->id_pertandingan) }}'>
+                                            See Details
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    @endforeach
+                @endif
             </div>
-          </div>
         </div>
-      
-      @endforeach
     </div>
-</div>
 
 @endsection
+
--- GolfCommunity/routes/web.php
+++ GolfCommunity/routes/web.php
@@ -26,4 +26,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('/pertandingan', 'PertandinganController@home');
 Route::get('/pertandingan/listpertandingan', 'PertandinganController@list');
 Route::get('/pertandingan/buatp', 'PertandinganController@buatp');
+Route::get('/pertandingan/showpertandingan', 'PertandinganController@show');
 Route::post('/pertandingan/form-pertandingan', [PertandinganController::class, 'buatpertandingan']);
