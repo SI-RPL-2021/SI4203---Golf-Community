0c2301dcfda729368508c6d35205615396665693
Amar11126 <71832845+Amar11126@users.noreply.github.com>
2021-06-27 21:17:35 +0700

    Buat Berita FIX

--- GolfCommunity/resources/views/showberita.blade.php
+++ GolfCommunity/resources/views/showberita.blade.php
@@ -3,6 +3,9 @@
 
 @section('konten')
 <div class="container">
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+ Buat Berita
+</button>
 @foreach ($news as $berita)
 <div class="col-md-4 col-sm-12 mt-4">
     <div class="card">
@@ -16,5 +19,39 @@
 </div>
 <br>
 @endforeach
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Berita</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="/berita/create" method="POST">
+        {{csrf_field()}}
+        <div class="form-group{{$errors->has('judul') ? ' has-error ' : ''}}">
+            <label for="exampleInputEmail">Judul</label>
+            <input name="judul" type="text" class="form-control" id="exampleInputEmail" value="{{old('judul')}}">
+            @if($errors->has('judul'))
+                <span class="help-block">{{$errors->first('judul')}}</span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Deskripsi</label>
+            <textarea name="deskripsi" class="form-control" id="deskripsi" rows="3"></textarea>
+        </div>
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Post</button>
+      </div>
+      </form>
+    </div>
+  </div>
 </div>
 @endsection
