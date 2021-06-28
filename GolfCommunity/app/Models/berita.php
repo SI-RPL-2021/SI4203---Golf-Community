d9b729d045e33c6f512392a0c437057e54d8cf06
Amar11126 <71832845+Amar11126@users.noreply.github.com>
2021-06-28 10:10:27 +0700

    tambahan

---
+++ GolfCommunity/app/Models/berita.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class berita extends Model
+{
+    use HasFactory;
+    protected $table = 'berita';
+    protected $fillable = [
+        'id',
+        'judul',
+        'deskripsi',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
